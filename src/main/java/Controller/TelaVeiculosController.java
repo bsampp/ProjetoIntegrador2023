c24ae8cc293409b2a9af97688a183fc3265f60dd
bsampp <b.sampietro02@gmail.com>
2023-02-28 22:58:08 -0300

    navegação telas

---
+++ src/main/java/Controller/TelaVeiculosController.java
@@ -0,0 +1,52 @@
+package Controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class TelaVeiculosController {
+
+    @FXML
+    private Button btnRegistrar;
+
+    @FXML
+    private Button btnVoltar;
+
+    @FXML
+    private TableColumn<?, ?> tcCodCliente;
+
+    @FXML
+    private TableColumn<?, ?> tcNomeCliente;
+
+    @FXML
+    private TableColumn<?, ?> tcTelefoneCliente;
+
+    @FXML
+    void btnRegistrarAction(ActionEvent event) throws IOException {
+        FXMLLoader Loader = new FXMLLoader(getClass().getResource("/application/cadastroVeiculo.fxml"));
+        Parent Root = Loader.load();
+        Scene Scene = new Scene(Root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(Scene);
+    }
+
+    @FXML
+    void btnVoltarAction(ActionEvent event) throws IOException {
+        FXMLLoader principal = new FXMLLoader(getClass().getResource("/application/telaPrincipal.fxml"));
+        principal.setRoot(new AnchorPane());
+        Parent root = principal.load();
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+    }
+
+}
